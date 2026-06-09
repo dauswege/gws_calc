ce755c6d306778625db8324c3ab6e4af937e4c49
David Ausweger <david.ausweger@porscheinformatik.com>
2026-06-09 14:44:21 +0200

    add missing files

---
+++ src/app/core/models/products.ts
@@ -0,0 +1,55 @@
+export const PRODUCTS = [
+  // Getränke
+  { id: '1', name: 'Bier / Radler / Weißbier alkoholfrei 1/2', price: 5.00, category: 'Getränke', active: true },
+  { id: '4', name: 'Weißbier 0,3', price: 4.00, category: 'Getränke', active: true },
+  { id: '5', name: 'Wein weiß / rot 1/8', price: 3.50, category: 'Getränke', active: true },
+  { id: '7', name: 'Wein Flasche weiß / rot', price: 16.00, category: 'Getränke', active: true },
+  { id: '9', name: 'Spritzer', price: 4.00, category: 'Getränke', active: true },
+  { id: '10', name: 'Sommerspritzer', price: 5.00, category: 'Getränke', active: true },
+  { id: '11', name: 'Limo', price: 3.00, category: 'Getränke', active: true },
+  { id: '12', name: 'Mineral 0,33l', price: 2.50, category: 'Getränke', active: true },
+  { id: '13', name: 'Mineral 1l', price: 4.00, category: 'Getränke', active: true },
+  { id: '14', name: 'Apfelsaft / Johannisbeer / Holler gespritzt', price: 4.00, category: 'Getränke', active: true },
+
+  // Speisen
+  { id: '17', name: 'Gegrillter Schopf mit Salat und Gebäck', price: 10.00, category: 'Speisen', active: true },
+  { id: '18', name: 'Hendl mit Pommes', price: 12.00, category: 'Speisen', active: true },
+  { id: '19', name: 'Grillwürstel mit Pommes', price: 6.00, category: 'Speisen', active: true },
+  { id: '20', name: 'Pommes Frites', price: 3.00, category: 'Speisen', active: true },
+
+  // Kaffee & Kuchen
+  { id: '21', name: 'Kaffee', price: 2.50, category: 'Kaffee & Kuchen', active: true },
+  { id: '22', name: 'Kuchen oder Torte', price: 3.00, category: 'Kaffee & Kuchen', active: true }
+];
+
+
+// export const PRODUCTS = [
+//   // Getränke
+//   { id: '1', name: 'Bier', price: 5.00, category: 'Getränke', active: true },
+//   { id: '2', name: 'Weißbier alkoholfrei', price: 5.00, category: 'Getränke', active: true },
+//   { id: '3', name: 'Radler', price: 5.00, category: 'Getränke', active: true },
+//   { id: '4', name: 'Weißbier', price: 4.00, category: 'Getränke', active: true },
+//   { id: '5', name: 'Wein weiß', price: 3.50, category: 'Getränke', active: true },
+//   { id: '6', name: 'Wein rot', price: 3.50, category: 'Getränke', active: true },
+//   { id: '7', name: 'Wein Flasche weiß', price: 16.00, category: 'Getränke', active: true },
+//   { id: '8', name: 'Wein Flasche rot', price: 16.00, category: 'Getränke', active: true },
+//   { id: '9', name: 'Spritzer', price: 4.00, category: 'Getränke', active: true },
+//   { id: '10', name: 'Sommerspritzer', price: 5.00, category: 'Getränke', active: true },
+//   { id: '11', name: 'Limo', price: 3.00, category: 'Getränke', active: true },
+//   { id: '12', name: 'Mineral 0,33l', price: 2.50, category: 'Getränke', active: true },
+//   { id: '13', name: 'Mineral 1l', price: 4.00, category: 'Getränke', active: true },
+//   { id: '14', name: 'Apfelsaft gespritzt', price: 4.00, category: 'Getränke', active: true },
+//   { id: '15', name: 'Johannisbeer gespritzt', price: 4.00, category: 'Getränke', active: true },
+//   { id: '16', name: 'Holler gespritzt', price: 4.00, category: 'Getränke', active: true },
+//
+//   // Speisen
+//   { id: '17', name: 'Gegrillter Schopf mit Salat und Gebäck', price: 10.00, category: 'Speisen', active: true },
+//   { id: '18', name: 'Hendl mit Pommes', price: 12.00, category: 'Speisen', active: true },
+//   { id: '19', name: 'Grillwürstel mit Pommes', price: 6.00, category: 'Speisen', active: true },
+//   { id: '20', name: 'Pommes Frites', price: 3.00, category: 'Speisen', active: true },
+//
+//   // Kaffee & Kuchen
+//   { id: '21', name: 'Kaffee', price: 2.50, category: 'Kaffee & Kuchen', active: true },
+//   { id: '22', name: 'Kuchen oder Torte', price: 3.00, category: 'Kaffee & Kuchen', active: true }
+// ];
+//
